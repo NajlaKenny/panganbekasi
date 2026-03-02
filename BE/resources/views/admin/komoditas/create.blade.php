0e609a0036a62bfeda23eb40921c3ff4f4252a48
Salmarani17 <salmaraniardelia@gmail.com>
2026-03-02 11:24:37 +0700

    Hasil Pekerjaan Data Barang

---
+++ BE/resources/views/admin/komoditas/create.blade.php
@@ -0,0 +1,59 @@
+<x-admin-layout>
+    <div class="max-w-2xl mx-auto">
+
+        <div class="mb-8 flex justify-between">
+            <div>
+                <h2 class="text-2xl font-bold text-gray-800">
+                    Tambah Data Barang
+                </h2>
+                <p class="text-gray-500">
+                    Masukkan Barang baru.
+                </p>
+            </div>
+
+            <a href="{{ route('komoditas.index') }}"
+                class="px-4 py-2 border rounded-lg">
+                ← Kembali
+            </a>
+        </div>
+
+        <div class="bg-white rounded-xl border shadow-sm">
+
+            <form method="POST"
+                action="{{ route('komoditas.store') }}"
+                class="p-6 space-y-6">
+
+                @csrf
+
+                <!-- Nama -->
+                <div>
+                    <label class="block mb-2 font-medium">
+                        Nama Barang
+                    </label>
+
+                    <input type="text"
+                        name="nama"
+                        value="{{ old('nama') }}"
+                        required
+                        class="w-full border rounded-lg px-4 py-2">
+
+                    @error('nama')
+                        <p class="text-red-600 mt-1">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex justify-end border-t pt-4">
+                    <button type="submit"
+                        class="px-6 py-2 text-white bg-indigo-600 rounded-lg hover:bg-indigo-700">
+                        Simpan
+                    </button>
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+</x-admin-layout>
---
+++ BE/resources/views/admin/komoditas/edit.blade.php
@@ -0,0 +1,45 @@
+<x-admin-layout>
+
+<div class="max-w-xl">
+
+    <h2 class="text-xl font-bold mb-5">
+        Edit Barang
+    </h2>
+
+    <form method="POST"
+        action="{{ route('komoditas.update', $komoditas) }}">
+
+        @csrf
+        @method('PUT')
+
+        <div class="mb-4">
+
+            <label class="block mb-2 font-medium">
+                Nama Barang
+            </label>
+
+            <input type="text"
+                name="nama"
+                value="{{ old('nama', $komoditas->nama) }}"
+                class="border p-2 w-full rounded">
+
+            @error('nama')
+                <p class="text-red-600 mt-1">
+                    {{ $message }}
+                </p>
+            @enderror
+
+        </div>
+
+        <button
+            class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600">
+
+            Update
+
+        </button>
+
+    </form>
+
+</div>
+
+</x-admin-layout>
